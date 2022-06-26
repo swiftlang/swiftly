6a500c6e191c81dffb1418f84ad3b1c0ecd20248
Patrick Freed <patrick.freed@mongodb.com>
2022-06-26 01:30:48 -0400

    split up toolchainversion and toolchainselectors

--- Sources/Swiftly/Install.swift
+++ Sources/Swiftly/Install.swift
@@ -7,80 +7,13 @@ struct Install: AsyncParsableCommand {
     @Argument(help: "The version of the toolchain to install.")
     var version: String
 
-    static let versionResolvers: [any ToolchainVersionResolver] = [
-        StableVersionResolver(),
-        ReleaseSnapshotResolver(),
-        MainSnapshotResolver(),
-    ]
-
     mutating func run() async throws {
-        let toolchainVersion = try await self.resolve()
+        let selector = try ToolchainSelector(parsing: self.version)
+        let toolchainVersion = try await self.resolve(selector: selector)
         print("installing \(toolchainVersion)")
     }
 
-    func resolve() async throws -> ToolchainVersion {
-        for resolver in Self.versionResolvers {
-            guard let resolvedVersion = try await resolver.resolve(version: version) else {
-                continue
-            }
-            return resolvedVersion
-        }
-        throw Error(message: "invalid version specification: \"\(self.version)\"")
-    }
-}
-
-protocol ToolchainVersionResolver {
-    func resolve(version: String) async throws -> ToolchainVersion?
-}
-
-/// Resolver for matching versions like the following:
-///    - a.b.c
-///    - a.b
-///
-/// If a patch version (the ".c" above) is omitted, a network lookup will be performed
-/// to resolve the version to the latest patch release associated with the given a.b
-/// major/minor version pair.
-struct StableVersionResolver: ToolchainVersionResolver {
-    /// Model of a GitHub REST API release object.
-    struct GitHubRelease: Decodable {
-        /// The name of the release.
-        /// e.g. "Swift a.b[.c] Release".
-        let name: String
-
-        func parse() throws -> ToolchainVersion.StableRelease {
-            // names look like Swift a.b.c Release
-            let parts = self.name.split(separator: " ")
-            guard parts.count >= 2 else {
-                throw Error(message: "Malformatted release name from GitHub API: \(self.name)")
-            }
-
-            // versions can be a.b.c or a.b for .0 releases
-            let versionParts = parts[1].split(separator: ".")
-            guard
-                versionParts.count >= 2,
-                let major = Int(versionParts[0]),
-                let minor = Int(versionParts[1])
-            else {
-                throw Error(message: "Malformatted release version from GitHub API: \(parts[1])")
-            }
-
-            let patch: Int
-            if versionParts.count == 3 {
-                guard let p = Int(versionParts[2]) else {
-                    throw Error(message: "Malformatted patch version from GitHub API: \(versionParts[2])")
-                }
-                patch = p
-            } else {
-                patch = 0
-            }
-
-            return ToolchainVersion.StableRelease(major: major, minor: minor, patch: patch)
-        }
-    }
-
-    static let regex: Regex<(Substring, Substring, Substring, Substring?)> = try! Regex("^(\\d+)\\.(\\d+)(?:\\.(\\d+))?$")
-
-    func getLatestReleases(numberOfReleases n: Int? = nil) async throws -> [GitHubRelease] {
+    private func getLatestReleases(numberOfReleases n: Int? = nil) async throws -> [GitHubRelease] {
         var url = "https://api.github.com/repos/apple/swift/releases"
         if let n {
             url += "?per_page=\(n)"
@@ -88,101 +21,78 @@ struct StableVersionResolver: ToolchainVersionResolver {
         return try await HTTP().getFromJSON(url: url, type: [GitHubRelease].self)
     }
 
-    func resolve(version: String) async throws -> ToolchainVersion? {
-        if version == "latest" {
+    func resolve(selector: ToolchainSelector) async throws -> ToolchainVersion {
+        switch selector {
+        case .latest:
+            // get the latest stable release
             guard let release = try await self.getLatestReleases(numberOfReleases: 1).first else {
-                return nil
+                throw Error(message: "couldnt get latest releases")
             }
             return try .stable(release.parse())
-        }
 
-        guard let matches = try Self.regex.wholeMatch(in: version) else {
-            return nil
-        }
-
-        let major = Int(matches.1)!
-        let minor = Int(matches.2)!
-
-        if let patch = matches.3 {
-            return .stable(ToolchainVersion.StableRelease(major: major, minor: minor, patch: Int(patch)!))
-        }
-
-        let releases = try await self.getLatestReleases()
-
-        for release in releases {
-            let parsed = try release.parse()
-            guard
-                parsed.major == major,
-                parsed.minor == minor
-            else {
-                continue
+        case let .stable(major, minor, patch):
+            if let patch {
+                return .stable(ToolchainVersion.StableRelease(major: major, minor: minor, patch: patch))
             }
-            return .stable(parsed)
-        }
 
-        throw Error(message: "No release found matching \(major).\(minor)")
+            // if no patch was provided, perform a network lookup to get the latest patch release
+            // of the provided major/minor version pair.
+            for release in try await self.getLatestReleases() {
+                let parsed = try release.parse()
+                guard
+                    parsed.major == major,
+                    parsed.minor == minor
+                else {
+                    continue
+                }
+                return .stable(parsed)
+            }
+
+            throw Error(message: "No release found matching \(major).\(minor)")
+
+        case let .snapshot(branch, date):
+            if let date {
+                return .snapshot(branch: branch, date: date)
+            }
+            // TODO: get latest snapshot if no date provided
+            throw Error(message: "TODO get latest snapshot")
+        }
     }
 }
 
-/// Resolver for versions like the following:
-///    - a.b-snapshot-YYYY-mm-dd
-///    - a.b-snapshot
-///    - a.b-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd-a
-///    - a.b-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd
-///    - a.b-DEVELOPMENT-SNAPSHOT
-///
-/// If a date is omitted, a network lookup will be performed to determine the newest
-/// snapshot associated with the a.b release.
-struct ReleaseSnapshotResolver: ToolchainVersionResolver {
-    static let regex: Regex<(Substring, Substring, Substring, Substring?)> =
-        try! Regex("^([0-9]+)\\.([0-9]+)-(?:snapshot|DEVELOPMENT-SNAPSHOT)(?:-([0-9]{4}-[0-9]{2}-[0-9]{2}))?(?:-a)?$")
+/// Model of a GitHub REST API release object.
+private struct GitHubRelease: Decodable {
+    /// The name of the release.
+    /// e.g. "Swift a.b[.c] Release".
+    let name: String
 
-    func resolve(version: String) async throws -> ToolchainVersion? {
-        guard let match = try Self.regex.wholeMatch(in: version) else {
-            return nil
+    func parse() throws -> ToolchainVersion.StableRelease {
+        // names look like Swift a.b.c Release
+        let parts = self.name.split(separator: " ")
+        guard parts.count >= 2 else {
+            throw Error(message: "Malformatted release name from GitHub API: \(self.name)")
         }
 
-        let major = Int(match.output.1)!
-        let minor = Int(match.output.2)!
+        // versions can be a.b.c or a.b for .0 releases
+        let versionParts = parts[1].split(separator: ".")
+        guard
+            versionParts.count >= 2,
+            let major = Int(versionParts[0]),
+            let minor = Int(versionParts[1])
+        else {
+            throw Error(message: "Malformatted release version from GitHub API: \(parts[1])")
+        }
 
-        let date: String
-        if let d = match.output.3 {
-            date = String(d)
+        let patch: Int
+        if versionParts.count == 3 {
+            guard let p = Int(versionParts[2]) else {
+                throw Error(message: "Malformatted patch version from GitHub API: \(versionParts[2])")
+            }
+            patch = p
         } else {
-            // TODO: get latest snapshot associated with release
-            throw Error(message: "TODO: get latest snapshot")
+            patch = 0
         }
 
-        return .snapshot(branch: .release(major: major, minor: minor), date: date)
-    }
-}
-
-/// Resolver for matching versions like the following:
-///    - main-snapshot-YYYY-mm-dd
-///    - main-snapshot
-///    - swift-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd-a
-///    - swift-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd
-///    - swift-DEVELOPMENT-SNAPSHOT
-///
-/// If a date is omitted, a network lookup will be performed to determine the newest
-/// main branch snapshot.
-struct MainSnapshotResolver: ToolchainVersionResolver {
-    static let regex: Regex<(Substring, Substring?)> =
-        try! Regex("^(?:main-snapshot|swift-DEVELOPMENT-SNAPSHOT)(?:-([0-9]{4}-[0-9]{2}-[0-9]{2}))?(?:-a)?$")
-
-    func resolve(version: String) async throws -> ToolchainVersion? {
-        guard let match = try Self.regex.wholeMatch(in: version) else {
-            return nil
-        }
-
-        let date: String
-        if let d = match.output.1 {
-            date = String(d)
-        } else {
-            // TODO: get latest snapshot associated with release
-            throw Error(message: "TODO: get latest snapshot")
-        }
-
-        return .snapshot(branch: .main, date: date)
+        return ToolchainVersion.StableRelease(major: major, minor: minor, patch: patch)
     }
 }
--- Sources/Swiftly/ToolchainVerison.swift
+++ Sources/Swiftly/ToolchainVerison.swift
@@ -1,3 +1,6 @@
+import _StringProcessing
+
+/// Enum representing a toolchain version.
 enum ToolchainVersion {
     enum SnapshotBranch {
         case main
@@ -24,5 +27,121 @@ extension ToolchainVersion: CustomStringConvertible {
         case let .snapshot(.release(major, minor), date):
             return "\(major).\(minor)-snapshot-\(date)"
         }
+    }
+}
+
+/// Enum modeling a partially or fully supplied selector of a toolchain version.
+enum ToolchainSelector {
+    /// Select the latest stable toolchain.
+    case latest
+
+    /// Select a specific stable release toolchain.
+    ///
+    /// If the patch is not provided, this will select the latest patch release
+    /// associated with the given major/minor version pair.
+    case stable(major: Int, minor: Int, patch: Int?)
+
+    /// Select a snapshot toolchain.
+    ///
+    /// If the date is not provided, this will select the latest snapshot
+    /// associated with the given branch.
+    case snapshot(branch: ToolchainVersion.SnapshotBranch, date: String?)
+
+    init(parsing input: String) throws {
+        for parser in parsers {
+            guard let selector = try parser.parse(input) else {
+                continue
+            }
+            self = selector
+            return
+        }
+
+        throw Error(message: "invalid toolchain selector: \"\(input)\"")
+    }
+}
+
+/// Protocol used to facilitate parsing `ToolchainSelector`s from strings.
+protocol ToolchainSelectorParser {
+    func parse(_ string: String) throws -> ToolchainSelector?
+}
+
+/// List of all the available selector parsers.
+private let parsers: [any ToolchainSelectorParser] = [
+    StableVersionParser(),
+    ReleaseSnapshotParser(),
+    MainSnapshotParser()
+]
+
+/// Parser for version selectors like the following:
+///    - latest
+///    - a.b.c
+///    - a.b
+struct StableVersionParser: ToolchainSelectorParser {
+    static let regex: Regex<(Substring, Substring, Substring, Substring?)> = try! Regex("^(\\d+)\\.(\\d+)(?:\\.(\\d+))?$")
+
+    func parse(_ input: String) throws -> ToolchainSelector? {
+        if input == "latest" {
+            return .latest
+        }
+
+        guard let match = try Self.regex.wholeMatch(in: input) else {
+            return nil
+        }
+
+        let major = Int(match.output.1)!
+        let minor = Int(match.output.2)!
+
+        if let patch = match.output.3 {
+            guard let patchNumber = Int(patch) else {
+                throw Error(message: "invalid patch version: \(patch)")
+            }
+            return .stable(major: major, minor: minor, patch: patchNumber)
+        } else {
+            return .stable(major: major, minor: minor, patch: nil)
+        }
+    }
+}
+
+/// Parser for selectors like the following:
+///    - a.b-snapshot-YYYY-mm-dd
+///    - a.b-snapshot
+///    - a.b-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd-a
+///    - a.b-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd
+///    - a.b-DEVELOPMENT-SNAPSHOT
+struct ReleaseSnapshotParser: ToolchainSelectorParser {
+    static let regex: Regex<(Substring, Substring, Substring, Substring?)> =
+        try! Regex("^([0-9]+)\\.([0-9]+)-(?:snapshot|DEVELOPMENT-SNAPSHOT)(?:-([0-9]{4}-[0-9]{2}-[0-9]{2}))?(?:-a)?$")
+
+    func parse(_ input: String) throws -> ToolchainSelector? {
+        guard let match = try Self.regex.wholeMatch(in: input) else {
+            return nil
+        }
+
+        guard
+            let major = Int(match.output.1),
+            let minor = Int(match.output.2)
+        else {
+            throw Error(message: "malformatted version: \(match.output.1).\(match.output.2)")
+        }
+
+        return .snapshot(branch: .release(major: major, minor: minor), date: match.output.3.map(String.init))
+    }
+}
+
+/// Parser for selectors like the following:
+///    - main-snapshot-YYYY-mm-dd
+///    - main-snapshot
+///    - swift-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd-a
+///    - swift-DEVELOPMENT-SNAPSHOT-YYYY-mm-dd
+///    - swift-DEVELOPMENT-SNAPSHOT
+struct MainSnapshotParser: ToolchainSelectorParser {
+    static let regex: Regex<(Substring, Substring?)> =
+        try! Regex("^(?:main-snapshot|swift-DEVELOPMENT-SNAPSHOT)(?:-([0-9]{4}-[0-9]{2}-[0-9]{2}))?(?:-a)?$")
+
+    func parse(_ input: String) throws -> ToolchainSelector? {
+        guard let match = try Self.regex.wholeMatch(in: input) else {
+            return nil
+        }
+        return .snapshot(branch: .main, date: match.output.1.map(String.init))
     }
 }

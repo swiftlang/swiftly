187c88752e1859195166e16fa231e6704030bdcf
Patrick Freed <patrick.freed@mongodb.com>
2022-07-18 16:08:04 -0400

    minor style fixes

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.7
 
 import class Foundation.ProcessInfo
 import PackageDescription
@@ -37,21 +36,17 @@ var linuxSwiftSettings: [SwiftSetting] = []
 let package = Package(
     name: "swiftly",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .executable(
             name: "swiftly",
             targets: ["Swiftly"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.2"),
         .package(url: "https://github.com/swift-server/async-http-client", from: "1.9.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.38.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Swiftly",
             dependencies: [

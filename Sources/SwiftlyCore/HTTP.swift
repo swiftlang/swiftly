7a4b370aeb96b0fe8564c1758eb8485d8eae8d7d
Patrick Freed <patrick.freed@mongodb.com>
2022-09-07 17:48:28 -0400

    flush file to disk in download

--- Sources/SwiftlyCore/HTTP.swift
+++ Sources/SwiftlyCore/HTTP.swift
@@ -220,6 +220,8 @@ public class HTTP {
             }
             reportProgress(DownloadProgress(receivedBytes: receivedBytes, totalBytes: expectedBytes))
         }
+
+        try fileHandle.synchronize()
     }
 }
 

fffb75909a5c1672c99dd8d6808b3fc6e7ba2a19
Patrick Freed <patrick.freed@mongodb.com>
2022-09-15 19:04:09 -0400

    move GitHub API specific logic to separate file

--- Sources/SwiftlyCore/HTTPClient.swift
+++ Sources/SwiftlyCore/HTTPClient.swift
@@ -9,9 +9,6 @@ import _StringProcessing
 public class HTTP {
     private static let client = HTTPClientWrapper()
 
-    /// The GitHub authentication token to use for any requests made to the GitHub API.
-    public static var githubToken: String? = nil
-
     private struct Response {
         let status: HTTPResponseStatus
         let buffer: ByteBuffer
@@ -57,62 +54,6 @@ public class HTTP {
         return try JSONDecoder().decode(type.self, from: response.buffer)
     }
 
-    /// Function used to iterate over pages of GitHub tags/releases and map + filter them down.
-    /// The `fetch` closure is used to retrieve the next page of results. It accepts the page number as its argument.
-    /// The `filterMap` closure maps the input GitHub tag to an output. If it returns nil, it will not be included
-    /// in the returned array.
-    private static func mapGithubTags<T>(
-        limit: Int?,
-        filterMap: ((GitHubTag) throws -> T?),
-        fetch: (Int) async throws -> [GitHubTag]
-    ) async throws -> [T] {
-        var page = 1
-        var found: [T] = []
-
-        let limit = limit ?? Int.max
-
-        while true {
-            let results = try await fetch(page)
-            guard !results.isEmpty else {
-                return found
-            }
-
-            for githubRelease in results {
-                guard let out = try filterMap(githubRelease) else {
-                    continue
-                }
-                found.append(out)
-
-                guard found.count < limit else {
-                    return found
-                }
-            }
-
-            page += 1
-        }
-    }
-
-    /// Get a JSON response from the GitHub REST API.
-    /// This will use the authorization token set, if any.
-    private static func getFromGitHub<T: Decodable>(url: String) async throws -> T {
-        var headers: [String: String] = [:]
-        if let token = Self.githubToken {
-            headers["Authorization"] = "Bearer \(token)"
-        }
-
-        return try await Self.getFromJSON(url: url, type: T.self, headers: headers)
-    }
-
-    /// Get a list of releases on the apple/swift GitHub repository.
-    /// The releases are returned in "pages" of `perPage` releases (default 100). The page argument specifies the
-    /// page number.
-    ///
-    /// The results are returned in lexicographic order.
-    private static func getReleases(page: Int, perPage: Int = 100) async throws -> [GitHubTag] {
-        let url = "https://api.github.com/repos/apple/swift/releases?per_page=\(perPage)&page=\(page)"
-        return try await Self.getFromGitHub(url: url)
-    }
-
     /// Return an array of released Swift versions that match the given filter, up to the provided
     /// limit (default unlimited).
     ///
@@ -138,15 +79,6 @@ public class HTTP {
         return try await Self.mapGithubTags(limit: limit, filterMap: filterMap) { page in
             try await Self.getReleases(page: page)
         }
-    }
-
-    /// Get a list of tags on the apple/swift GitHub repository.
-    /// The tags are returned in pages of 100. The page argument specifies the page number.
-    ///
-    /// The results are returned in lexicographic order.
-    private static func getTags(page: Int) async throws -> [GitHubTag] {
-        let url = "https://api.github.com/repos/apple/swift/tags?per_page=100&page=\(page)"
-        return try await Self.getFromGitHub(url: url)
     }
 
     /// Return an array of Swift snapshots that match the given filter, up to the provided
@@ -230,63 +162,5 @@ private class HTTPClientWrapper {
 
     deinit {
         try? self.inner.syncShutdown()
-    }
-}
-
-/// Model of a GitHub REST API tag/release object.
-private struct GitHubTag: Decodable {
-    /// The name of the release.
-    /// e.g. "Swift a.b[.c] Release" or "swift-5.7-DEVELOPMENT-SNAPSHOT-2022-08-30-a".
-    let name: String
-
-    fileprivate func parseStableRelease() throws -> ToolchainVersion.StableRelease? {
-        // names look like Swift a.b.c Release
-        let parts = self.name.split(separator: " ")
-        guard parts.count >= 2 else {
-            return nil
-        }
-
-        // versions can be a.b.c or a.b for .0 releases
-        let versionParts = parts[1].split(separator: ".")
-        guard
-            versionParts.count >= 2,
-            let major = Int(versionParts[0]),
-            let minor = Int(versionParts[1])
-        else {
-            return nil
-        }
-
-        let patch: Int
-        if versionParts.count == 3 {
-            guard let p = Int(versionParts[2]) else {
-                return nil
-            }
-            patch = p
-        } else {
-            patch = 0
-        }
-
-        return ToolchainVersion.StableRelease(major: major, minor: minor, patch: patch)
-    }
-
-    private static let snapshotRegex: Regex<(Substring, Substring?, Substring?, Substring)> =
-        try! Regex("swift(?:-(\\d+)\\.(\\d+))?-DEVELOPMENT-SNAPSHOT-(\\d{4}-\\d{2}-\\d{2})")
-
-    fileprivate func parseSnapshot() throws -> ToolchainVersion.Snapshot? {
-        guard let match = try Self.snapshotRegex.firstMatch(in: self.name) else {
-            return nil
-        }
-
-        let branch: ToolchainVersion.Snapshot.Branch
-        if let majorString = match.output.1, let minorString = match.output.2 {
-            guard let major = Int(majorString), let minor = Int(minorString) else {
-                throw Error(message: "malformatted release branch: \"\(majorString).\(minorString)\"")
-            }
-            branch = .release(major: major, minor: minor)
-        } else {
-            branch = .main
-        }
-
-        return ToolchainVersion.Snapshot(branch: branch, date: String(match.output.3))
     }
 }

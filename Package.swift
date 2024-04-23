92339d6062d86fb680ae7bf36116e9ab12aefd4f
Adam Fowler <adamfowler71@gmail.com>
2024-04-23 03:53:09 +0100

    Update package versions (#98)

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.2"),
-        .package(url: "https://github.com/swift-server/async-http-client", from: "1.9.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.38.0"),
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.2.7"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/swift-server/async-http-client", from: "1.21.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.64.0"),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.6.1"),
     ],
     targets: [
         .executableTarget(

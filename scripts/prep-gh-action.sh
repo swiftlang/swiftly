e5c1da0c772d6ba2ad06bbc0039b515712ed0b52
Chris (SPG) McGee <christie_mcgee@apple.com>
2024-11-07 17:59:23 -0500

    Add appropriate libstdc++ to images that are missing them, and gnupg

---
+++ scripts/prep-gh-action.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get --help && apt-get update && apt-get -y install curl make
+yum --help && (curl --help && yum -y install curl) && yum install make
+
+(cat /etc/os-release | grep bookworm) && apt-get -y install libstdc++-12-dev gnupg2
+(cat /etc/os-release | grep 'Fedora Linux 39') && yum -y install libstdc++-devel
+
+exit 0

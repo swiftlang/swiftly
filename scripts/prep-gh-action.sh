c202b02bd21be0454bda74a6316bd2a1b1a30eb2
Chris (SPG) McGee <christie_mcgee@apple.com>
2024-11-07 21:37:12 -0500

    Add libstdc++-static package to fedora39 to fix a linker error

--- scripts/prep-gh-action.sh
+++ scripts/prep-gh-action.sh
@@ -4,6 +4,6 @@ apt-get --help && apt-get update && apt-get -y install curl make
 yum --help && (curl --help && yum -y install curl) && yum install make
 
 (cat /etc/os-release | grep bookworm) && apt-get -y install libstdc++-12-dev gnupg2
-(cat /etc/os-release | grep 'Fedora Linux 39') && yum -y install libstdc++-devel
+(cat /etc/os-release | grep 'Fedora Linux 39') && yum -y install libstdc++-devel libstdc++-static
 
 exit 0

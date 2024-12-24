19b69839331dd604089a8252670bf645af6b30f3
Chris (SPG) McGee <christie_mcgee@apple.com>
2024-12-24 09:21:51 -0500

    Add gpg to the action prep script

--- scripts/prep-gh-action.sh
+++ scripts/prep-gh-action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get --help && apt-get update && apt-get -y install curl make
-yum --help && (curl --help && yum -y install curl) && yum install make
+apt-get --help && apt-get update && apt-get -y install curl make gpg
+yum --help && (curl --help && yum -y install curl) && yum install make gpg
 
 (cat /etc/os-release | grep bookworm) && apt-get -y install libstdc++-12-dev gnupg2
 (cat /etc/os-release | grep 'Fedora Linux 39') && yum -y install libstdc++-devel libstdc++-static

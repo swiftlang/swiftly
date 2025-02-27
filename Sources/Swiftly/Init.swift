cad1353f1e04f8ad9dc47ede24b6cf18593b2fff
Chris McGee <87777443+cmcgee1024@users.noreply.github.com>
2025-02-27 17:41:26 -0500

    Update swiftly version to 1.0.0 and add upgrade routine for 0.4.0 (#237)

--- Sources/Swiftly/Init.swift
+++ Sources/Swiftly/Init.swift
@@ -37,8 +37,13 @@ internal struct Init: SwiftlyCommand {
 
         var config = try? Config.load()
 
-        if var config, !overwrite && config.version == SwiftlyVersion(major: 0, minor: 4, patch: 0, suffix: "dev") {
-            // This is a simple upgrade from the 0.4.0-dev pre-release
+        if var config, !overwrite &&
+            (
+                config.version == SwiftlyVersion(major: 0, minor: 4, patch: 0, suffix: "dev") ||
+                    config.version == SwiftlyVersion(major: 0, minor: 4, patch: 0)
+            )
+        {
+            // This is a simple upgrade from the 0.4.0-dev pre-release, or 0.4.0 release
 
             // Move our executable over to the correct place
             try Swiftly.currentPlatform.installSwiftlyBin()

5a0b96b7491b88504f41eb7dc15d9cab1eb34153
Chris McGee <87777443+cmcgee1024@users.noreply.github.com>
2025-03-11 13:27:11 -0400

    Make updates better and more resilient (#245)
    Put a check in-place to unset the global default toolchain if it is no longer installed
    
    Set the global default to the installed toolchain if it is not set
    
    Add full toolchain selection resolution to the update operation resolve update parameters
    
    Fix the use command and toolchain selection routine to consider a global default set to a toolchain that is not installed as no selection at all
    
    Add check for the physical presence of a toolchain to proxy so that it prevents circularity errors and provides an actionable message
    
    Allow uninstalling of partially installed toolchains from config.json
    
    Control verbosity of uninstall operation on macOS and other platforms

--- Tests/SwiftlyTests/PlatformTests.swift
+++ Tests/SwiftlyTests/PlatformTests.swift
@@ -53,21 +53,21 @@ final class PlatformTests: SwiftlyTests {
             (mockedToolchainFile, version) = try await self.mockToolchainDownload(version: "5.6.3")
             try Swiftly.currentPlatform.install(from: mockedToolchainFile, version: version, verbose: true)
             // WHEN: one of the toolchains is uninstalled
-            try Swiftly.currentPlatform.uninstall(version)
+            try Swiftly.currentPlatform.uninstall(version, verbose: true)
             // THEN: there is only one remaining toolchain installed
             var toolchains = try FileManager.default.contentsOfDirectory(at: Swiftly.currentPlatform.swiftlyToolchainsDir, includingPropertiesForKeys: nil)
             XCTAssertEqual(1, toolchains.count)
 
             // GIVEN; there is only one toolchain installed
             // WHEN: a non-existent toolchain is uninstalled
-            try? Swiftly.currentPlatform.uninstall(ToolchainVersion(parsing: "5.9.1"))
+            try? Swiftly.currentPlatform.uninstall(ToolchainVersion(parsing: "5.9.1"), verbose: true)
             // THEN: there is the one remaining toolchain that is still installed
             toolchains = try FileManager.default.contentsOfDirectory(at: Swiftly.currentPlatform.swiftlyToolchainsDir, includingPropertiesForKeys: nil)
             XCTAssertEqual(1, toolchains.count)
 
             // GIVEN: there is only one toolchain installed
             // WHEN: the last toolchain is uninstalled
-            try Swiftly.currentPlatform.uninstall(ToolchainVersion(parsing: "5.8.0"))
+            try Swiftly.currentPlatform.uninstall(ToolchainVersion(parsing: "5.8.0"), verbose: true)
             // THEN: there are no toolchains installed
             toolchains = try FileManager.default.contentsOfDirectory(at: Swiftly.currentPlatform.swiftlyToolchainsDir, includingPropertiesForKeys: nil)
             XCTAssertEqual(0, toolchains.count)

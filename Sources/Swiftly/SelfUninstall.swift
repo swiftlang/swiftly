30af030bc795fab25a2536b3f10e730f86ffb4f8
Louis Qian <yuq4@illinois.edu>
2025-05-05 16:32:55 -0500

    chore: lint

--- Sources/Swiftly/SelfUninstall.swift
+++ Sources/Swiftly/SelfUninstall.swift
@@ -1,7 +1,6 @@
-import Foundation
 import ArgumentParser
+import Foundation
 import SwiftlyCore
-
 
 struct SelfUninstall: SwiftlyCommand {
     public static let configuration = CommandConfiguration(
@@ -21,7 +20,6 @@ struct SelfUninstall: SwiftlyCommand {
     mutating func run(_ ctx: SwiftlyCoreContext) async throws {
         let _ = try await validateSwiftly(ctx)
         let swiftlyBin = Swiftly.currentPlatform.swiftlyBinDir(ctx)
-        let swiftlyHome = Swiftly.currentPlatform.swiftlyHomeDir(ctx)
 
         guard try await fs.exists(atPath: swiftlyBin) else {
             throw SwiftlyError(
@@ -33,7 +31,7 @@ struct SelfUninstall: SwiftlyCommand {
         try await Self.execute(ctx, verbose: self.root.verbose)
     }
 
-    public static func execute(_ ctx: SwiftlyCoreContext, verbose: Bool) async throws {
+    public static func execute(_ ctx: SwiftlyCoreContext, verbose _: Bool) async throws {
         await ctx.print("""
         You are about to uninstall swiftly. 
         This will remove the swiftly binary and all the files in the swiftly home directory. 
@@ -49,10 +47,10 @@ struct SelfUninstall: SwiftlyCommand {
         let swiftlyHome = Swiftly.currentPlatform.swiftlyHomeDir(ctx)
 
         await ctx.print("Removing swiftly binary from \(swiftlyBin)...")
-        // try await fs.remove(atPath: swiftlyBin)
+        try await fs.remove(atPath: swiftlyBin)
 
         await ctx.print("Removing swiftly home directory from \(swiftlyHome)...")
-        // try await fs.remove(atPath: swiftlyHome)
+        try await fs.remove(atPath: swiftlyHome)
 
         await ctx.print("Swiftly uninstalled successfully.")
     }

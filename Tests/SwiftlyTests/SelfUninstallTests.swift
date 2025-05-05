98f9a58b4d85a1de0483d63bdb1fca57e0823ad5
Louis Qian <yuq4@illinois.edu>
2025-05-05 18:24:31 -0500

    test: add expects & skeleton for more

--- Tests/SwiftlyTests/SelfUninstallTests.swift
+++ Tests/SwiftlyTests/SelfUninstallTests.swift
@@ -10,6 +10,14 @@ import Testing
         try await SwiftlyTests.withTestHome {
             let swiftlyBinDir = Swiftly.currentPlatform.swiftlyBinDir(SwiftlyTests.ctx)
             let swiftlyHomeDir = Swiftly.currentPlatform.swiftlyHomeDir(SwiftlyTests.ctx)
+            #expect(
+                try await fs.exists(atPath: swiftlyBinDir) == true,
+                "swiftly bin directory should exist"
+            )
+            #expect(
+                try await fs.exists(atPath: swiftlyHomeDir) == true,
+                "swiftly home directory should exist"
+            )
 
             try await SwiftlyTests.runCommand(SelfUninstall.self, ["self-uninstall"])
 
@@ -24,51 +32,11 @@ import Testing
         }
     }
 
-    // @Test(.mockedSwiftlyVersion(), .testHome(), arguments: [
-    //     "/bin/bash",
-    //     "/bin/zsh",
-    //     "/bin/fish",
-    // ]) func removesEntryFromShell(_ shell: String) async throws {
-    //     var ctx = SwiftlyTests.ctx
-    //     ctx.mockedShell = shell
-
-    //     try await SwiftlyTests.$ctx.withValue(ctx) {
-    //         let envScript: FilePath?
-    //         if shell.hasSuffix("bash") || shell.hasSuffix("zsh") {
-    //             envScript = Swiftly.currentPlatform.swiftlyHomeDir(SwiftlyTests.ctx) / "env.sh"
-    //         } else if shell.hasSuffix("fish") {
-    //             envScript = Swiftly.currentPlatform.swiftlyHomeDir(SwiftlyTests.ctx) / "env.fish"
-    //         } else {
-    //             envScript = nil
-    //         }
-
-    //         // if let envScript {
-    //         //     print(envScript.string)
-    //         // }
-
-    //         // WHEN: swiftly is invoked to uninstall
-    //         try await SwiftlyTests.runCommand(SelfUninstall.self, ["self-uninstall"])
-
-    //         // AND: it removes the source line from the user profile
-    //             // var sourceLineExist = false
-    //             for p in [
-    //                 ".profile",
-    //                 ".zprofile",
-    //                 ".bash_profile",
-    //                 ".bash_login",
-    //                 ".config/fish/conf.d/swiftly.fish",
-    //             ] {
-    //                 let profile = SwiftlyTests.ctx.mockedHomeDir! / p
-    //                 if try await fs.exists(atPath: profile) {
-    //                     // print profile contents only
-    //                     if let profileContents = try? String(contentsOf: profile) {
-    //                         print("contents of profile \(profileContents)")
-    //                         // sourceLineExist = profileContents.contains(envScript.string)
-    //                     }
-
-    //                 }
-    //             }
-    //             // #expect(sourceLineExist == false, "source line should be removed from the profile")
-    //     }
-    // }
+    @Test(.testHome(), arguments: [
+        "/bin/bash",
+        "/bin/zsh",
+        "/bin/fish",
+    ]) func removesEntryFromShellProfile(_ shell: String) async throws {
+        #expect(true)
+    }
 }

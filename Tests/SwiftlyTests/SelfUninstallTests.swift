6007d9749b0adb8d11e1fa8c27131d770367cff4
Louis Qian <yuq4@illinois.edu>
2025-05-06 20:56:02 -0500

    test: removesEntryFromShellProfile tests

--- Tests/SwiftlyTests/SelfUninstallTests.swift
+++ Tests/SwiftlyTests/SelfUninstallTests.swift
@@ -32,11 +32,85 @@ import Testing
         }
     }
 
-    @Test(.testHome(), arguments: [
+    @Test(.mockedSwiftlyVersion(), .testHome(), arguments: [
         "/bin/bash",
         "/bin/zsh",
         "/bin/fish",
-    ]) func removesEntryFromShellProfile(_: String) async throws {
-        #expect(true)
+    ]) func removesEntryFromShellProfile(_ shell: String) async throws {
+        var ctx = SwiftlyTests.ctx
+        ctx.mockedShell = shell
+
+        try await SwiftlyTests.$ctx.withValue(ctx) {
+            // Create a profile file with the source line
+            let userHome = SwiftlyTests.ctx.mockedHomeDir!
+
+            let profileHome: FilePath
+            if shell.hasSuffix("zsh") {
+                profileHome = userHome / ".zprofile"
+            } else if shell.hasSuffix("bash") {
+                if case let p = userHome / ".bash_profile", try await fs.exists(atPath: p) {
+                    profileHome = p
+                } else if case let p = userHome / ".bash_login", try await fs.exists(atPath: p) {
+                    profileHome = p
+                } else {
+                    profileHome = userHome / ".profile"
+                }
+            } else if shell.hasSuffix("fish") {
+                if let xdgConfigHome = ProcessInfo.processInfo.environment["XDG_CONFIG_HOME"], case let xdgConfigURL = FilePath(xdgConfigHome) {
+                    let confDir = xdgConfigURL / "fish/conf.d"
+                    try await fs.mkdir(.parents, atPath: confDir)
+                    profileHome = confDir / "swiftly.fish"
+                } else {
+                    let confDir = userHome / ".config/fish/conf.d"
+                    try await fs.mkdir(.parents, atPath: confDir)
+                    profileHome = confDir / "swiftly.fish"
+                }
+            } else {
+                profileHome = userHome / ".profile"
+            }
+
+            let envFile: FilePath
+            let sourceLine: String
+            if shell.hasSuffix("fish") {
+                envFile = Swiftly.currentPlatform.swiftlyHomeDir(ctx) / "env.fish"
+                sourceLine = """
+
+                # Added by swiftly
+                source "\(envFile)"
+                """
+            } else {
+                envFile = Swiftly.currentPlatform.swiftlyHomeDir(ctx) / "env.sh"
+                sourceLine = """
+
+                # Added by swiftly
+                . "\(envFile)"
+                """
+            }
+
+            let shellProfileContents = """
+            some other line before
+            \(sourceLine)
+            some other line after
+            """
+
+            try Data(shellProfileContents.utf8).write(to: profileHome)
+
+            // then call swiftly uninstall
+            try await SwiftlyTests.runCommand(SelfUninstall.self, ["self-uninstall"])
+
+            var sourceLineRemoved = true
+            for p in [".profile", ".zprofile", ".bash_profile", ".bash_login", ".config/fish/conf.d/swiftly.fish"] {
+                let profile = SwiftlyTests.ctx.mockedHomeDir! / p
+                if try await fs.exists(atPath: profile) {
+                    if let profileContents = try? String(contentsOf: profile), profileContents.contains(sourceLine) {
+                        // expect only the source line is removed
+                        #expect(profileContents == shellProfileContents.replacingOccurrences(of: sourceLine, with: ""))
+                        sourceLineRemoved = false
+                        break
+                    }
+                }
+            }
+            #expect(sourceLineRemoved, "swiftly should be removed from the profile file")
+        }
     }
 }

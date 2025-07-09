c68e151e55e113d46dddb13c35038551d1708e3c
Chris McGee <christie_mcgee@apple.com>
2025-07-09 08:53:12 -0400

    Add a test for self-update to user-specified version

--- Tests/SwiftlyTests/SelfUpdateTests.swift
+++ Tests/SwiftlyTests/SelfUpdateTests.swift
@@ -18,6 +18,10 @@ import Testing
         SwiftlyVersion(major: SwiftlyCore.version.major, minor: SwiftlyCore.version.minor, patch: SwiftlyCore.version.patch + 1)
     }
 
+    private static var newDevVersion: SwiftlyVersion {
+        SwiftlyVersion(major: SwiftlyCore.version.major, minor: SwiftlyCore.version.minor, patch: SwiftlyCore.version.patch + 1, suffix: "dev")
+    }
+
     func runSelfUpdateTest(latestVersion: SwiftlyVersion) async throws {
         try await SwiftlyTests.withTestHome {
             try await SwiftlyTests.withMockedSwiftlyVersion(latestSwiftlyVersion: latestVersion) {
@@ -36,5 +40,27 @@ import Testing
     /// Verify updating the most up-to-date toolchain has no effect.
     @Test func selfUpdateAlreadyUpToDate() async throws {
         try await self.runSelfUpdateTest(latestVersion: SwiftlyCore.version)
+    }
+
+    @Test func selfUpdateToUserSpecifiedVersion() async throws {
+        try await SwiftlyTests.withTestHome {
+            // GIVEN: swiftly is installed, and at the latest published version
+            try await SwiftlyTests.withMockedSwiftlyVersion(latestSwiftlyVersion: SwiftlyCore.version) {
+                // WHEN: An attempt is made to self-update to an equal version
+                var updatedVersion = try await SelfUpdate.execute(SwiftlyTests.ctx, verbose: true, version: SwiftlyCore.version)
+                // THEN: There is no change to the swiftly version
+                #expect(updatedVersion == SwiftlyCore.version)
+
+                // WHEN: An attempt is made to self-update to an older version
+                updatedVersion = try await SelfUpdate.execute(SwiftlyTests.ctx, verbose: true, version: SwiftlyVersion(major: SwiftlyCore.version.major - 1, minor: 0, patch: 0))
+                // THEN: There is no change to the swiftly version
+                #expect(updatedVersion == SwiftlyCore.version)
+
+                // WHEN: An attempt is made to self-update to a newer development version
+                updatedVersion = try await SelfUpdate.execute(SwiftlyTests.ctx, verbose: true, version: Self.newDevVersion)
+                // THEN: swiftly is updated to the new version
+                #expect(updatedVersion == Self.newDevVersion)
+            }
+        }
     }
 }

7bf6a5ddc3448e544ce9b661ab1d812b3c82d3e4
Chris McGee <cmcgee1024@gmail.com>
2025-10-17 16:47:56 -0400

    Add compatible configuration versions to init command for updates

--- Sources/Swiftly/Init.swift
+++ Sources/Swiftly/Init.swift
@@ -48,7 +48,9 @@ struct Init: SwiftlyCommand {
             (
                 config.version == SwiftlyVersion(major: 0, minor: 4, patch: 0, suffix: "dev") ||
                     config.version == SwiftlyVersion(major: 0, minor: 4, patch: 0) ||
-                    (config.version.major == 1 && config.version.minor == 0)
+                    (config.version.major == 1 && config.version.minor == 0) ||
+                    (config.version.major == 1 && config.version.minor == 1) ||
+                    (config.version.major == 2 && config.version.minor == 0)
             )
         {
             // This is a simple upgrade from the 0.4.0 pre-releases, or 1.x

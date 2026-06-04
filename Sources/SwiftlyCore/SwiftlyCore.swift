02c43bc590d6631411e3ada25faa20b1bd528bd8
Mishal Shah <mishal_shah@apple.com>
2026-06-04 08:09:05 -0700

    Update version to 1.1.2

--- Sources/SwiftlyCore/SwiftlyCore.swift
+++ Sources/SwiftlyCore/SwiftlyCore.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftlyWebsiteAPI
 import SystemPackage
 
-public let version = SwiftlyVersion(major: 1, minor: 1, patch: 1)
+public let version = SwiftlyVersion(major: 1, minor: 1, patch: 2)
 
 /// Protocol defining a handler for information swiftly intends to print to stdout.
 /// This is currently only used to intercept print statements for testing.

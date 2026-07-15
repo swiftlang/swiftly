ea49a9f7abcc6802395b49a3372fcf6dbc1bc07f
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-07-16 01:25:07 +0900

    Fix duplicate PATH removal with `fish_add_path`

--- Sources/Swiftly/Init.swift
+++ Sources/Swiftly/Init.swift
@@ -114,7 +114,8 @@ struct Init: SwiftlyCommand {
             set -x SWIFTLY_TOOLCHAINS_DIR "\(Swiftly.currentPlatform.swiftlyToolchainsDir(ctx))"
 
             # Remove SWIFTLY_BIN_DIR from PATH if present, then prepend it
-            fish_add_path -mP "$SWIFTLY_BIN_DIR"
+            set -gx PATH (string match -v -- "$SWIFTLY_BIN_DIR" $PATH)
+            fish_add_path -P "$SWIFTLY_BIN_DIR"
 
             """
         }

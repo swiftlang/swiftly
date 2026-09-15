04bb6575d4b5e7f97b73137ef8d0052c7b5ced88
Evan Wilde <ewilde@apple.com>
2026-09-15 09:06:02 -0700

    🍒Fix command expansion in env.sh (#591)
    * Escape SWIFTLY_* env vars
    
    Teaching Swiftly to escape the `SWIFTLY_*` environment variables and
    wrap them with single quotes.
    
    * Auto-update env.sh
    
    Updating unmodified env.sh files to use the single quotes.
    
    * Test: Verify shell escapes
    
    Adding tests to verify that the environment variables are single-quoted
    appropriately and that Swiftly upgrades from both old formats correctly.

--- Sources/Swiftly/Init.swift
+++ Sources/Swiftly/Init.swift
@@ -67,13 +67,21 @@ struct Init: SwiftlyCommand {
     static func execute(_ ctx: SwiftlyCoreContext, assumeYes: Bool, noModifyProfile: Bool, overwrite: Bool, platform: String?, verbose: Bool, skipInstall: Bool, quietShellFollowup: Bool) async throws {
         try await Swiftly.currentPlatform.verifySwiftlySystemPrerequisites()
 
+        let homeDirRaw = Swiftly.currentPlatform.swiftlyHomeDir(ctx)
+        let binDirRaw = Swiftly.currentPlatform.swiftlyBinDir(ctx)
+        let toolchainsDirRaw = Swiftly.currentPlatform.swiftlyToolchainsDir(ctx)
+
+        let homeDir = Swiftly.currentPlatform.escapePathForShell(homeDirRaw)
+        let binDir = Swiftly.currentPlatform.escapePathForShell(binDirRaw)
+        let toolchainsDir = Swiftly.currentPlatform.escapePathForShell(toolchainsDirRaw)
+
         var config = try? await Config.load(ctx)
 
-        func oldEnvSh(_ ctx: SwiftlyCoreContext) -> String {
+        func oldEnvSh2(_: SwiftlyCoreContext) -> String {
             """
-            export SWIFTLY_HOME_DIR="\(Swiftly.currentPlatform.swiftlyHomeDir(ctx))"
-            export SWIFTLY_BIN_DIR="\(Swiftly.currentPlatform.swiftlyBinDir(ctx))"
-            export SWIFTLY_TOOLCHAINS_DIR="\(Swiftly.currentPlatform.swiftlyToolchainsDir(ctx))"
+            export SWIFTLY_HOME_DIR="\(homeDirRaw)"
+            export SWIFTLY_BIN_DIR="\(binDirRaw)"
+            export SWIFTLY_TOOLCHAINS_DIR="\(toolchainsDirRaw)"
             if [[ ":$PATH:" != *":$SWIFTLY_BIN_DIR:"* ]]; then
                 export PATH="$SWIFTLY_BIN_DIR:$PATH"
             fi
@@ -81,11 +89,11 @@ struct Init: SwiftlyCommand {
             """
         }
 
-        func oldEnvFish(_ ctx: SwiftlyCoreContext) -> String {
+        func oldEnvFish2(_: SwiftlyCoreContext) -> String {
             """
-            set -x SWIFTLY_HOME_DIR "\(Swiftly.currentPlatform.swiftlyHomeDir(ctx))"
-            set -x SWIFTLY_BIN_DIR "\(Swiftly.currentPlatform.swiftlyBinDir(ctx))"
-            set -x SWIFTLY_TOOLCHAINS_DIR "\(Swiftly.currentPlatform.swiftlyToolchainsDir(ctx))"
+            set -x SWIFTLY_HOME_DIR "\(homeDirRaw)"
+            set -x SWIFTLY_BIN_DIR "\(binDirRaw)"
+            set -x SWIFTLY_TOOLCHAINS_DIR "\(toolchainsDirRaw)"
             if not contains "$SWIFTLY_BIN_DIR" $PATH
                 set -x PATH "$SWIFTLY_BIN_DIR" $PATH
             end
@@ -93,11 +101,11 @@ struct Init: SwiftlyCommand {
             """
         }
 
-        func envSh(_ ctx: SwiftlyCoreContext) -> String {
+        func oldEnvSh(_: SwiftlyCoreContext) -> String {
             """
-            export SWIFTLY_HOME_DIR="\(Swiftly.currentPlatform.swiftlyHomeDir(ctx))"
-            export SWIFTLY_BIN_DIR="\(Swiftly.currentPlatform.swiftlyBinDir(ctx))"
-            export SWIFTLY_TOOLCHAINS_DIR="\(Swiftly.currentPlatform.swiftlyToolchainsDir(ctx))"
+            export SWIFTLY_HOME_DIR="\(homeDirRaw)"
+            export SWIFTLY_BIN_DIR="\(binDirRaw)"
+            export SWIFTLY_TOOLCHAINS_DIR="\(toolchainsDirRaw)"
 
             # Remove SWIFTLY_BIN_DIR from PATH if present, then prepend it
             PATH="${PATH//:$SWIFTLY_BIN_DIR/}"
@@ -107,11 +115,40 @@ struct Init: SwiftlyCommand {
             """
         }
 
-        func envFish(_ ctx: SwiftlyCoreContext) -> String {
+        func oldEnvFish(_: SwiftlyCoreContext) -> String {
             """
-            set -x SWIFTLY_HOME_DIR "\(Swiftly.currentPlatform.swiftlyHomeDir(ctx))"
-            set -x SWIFTLY_BIN_DIR "\(Swiftly.currentPlatform.swiftlyBinDir(ctx))"
-            set -x SWIFTLY_TOOLCHAINS_DIR "\(Swiftly.currentPlatform.swiftlyToolchainsDir(ctx))"
+            set -x SWIFTLY_HOME_DIR "\(homeDirRaw)"
+            set -x SWIFTLY_BIN_DIR "\(binDirRaw)"
+            set -x SWIFTLY_TOOLCHAINS_DIR "\(toolchainsDirRaw)"
+
+            # Remove SWIFTLY_BIN_DIR from PATH if present, then prepend it
+            while set -l index (contains -i "$SWIFTLY_BIN_DIR" $PATH)
+                set -e PATH[$index]
+            end
+            set -x PATH "$SWIFTLY_BIN_DIR" $PATH
+
+            """
+        }
+
+        func envSh(_: SwiftlyCoreContext) -> String {
+            """
+            export SWIFTLY_HOME_DIR=\(homeDir)
+            export SWIFTLY_BIN_DIR=\(binDir)
+            export SWIFTLY_TOOLCHAINS_DIR=\(toolchainsDir)
+
+            # Remove SWIFTLY_BIN_DIR from PATH if present, then prepend it
+            PATH="${PATH//:$SWIFTLY_BIN_DIR/}"
+            PATH="${PATH/#$SWIFTLY_BIN_DIR:/}"
+            export PATH="$SWIFTLY_BIN_DIR:$PATH"
+
+            """
+        }
+
+        func envFish(_: SwiftlyCoreContext) -> String {
+            """
+            set -x SWIFTLY_HOME_DIR \(homeDir)
+            set -x SWIFTLY_BIN_DIR \(binDir)
+            set -x SWIFTLY_TOOLCHAINS_DIR \(toolchainsDir)
 
             # Remove SWIFTLY_BIN_DIR from PATH if present, then prepend it
             while set -l index (contains -i "$SWIFTLY_BIN_DIR" $PATH)
@@ -132,7 +169,7 @@ struct Init: SwiftlyCommand {
             if case let envFile = (Swiftly.currentPlatform.swiftlyHomeDir(ctx)) / "env.sh",
                (try? await fs.exists(atPath: envFile)) ?? false,
                let contents = String(data: (try? await fs.cat(atPath: envFile)) ?? Data(), encoding: .utf8),
-               contents == oldEnvSh(ctx)
+               contents == oldEnvSh(ctx) || contents == oldEnvSh2(ctx)
             {
                 await ctx.print("Updating shell environment \(envFile)")
                 try Data(envSh(ctx).utf8).write(to: envFile, options: .atomic)
@@ -142,7 +179,7 @@ struct Init: SwiftlyCommand {
             if case let envFile = (Swiftly.currentPlatform.swiftlyHomeDir(ctx)) / "env.fish",
                (try? await fs.exists(atPath: envFile)) ?? false,
                let contents = String(data: (try? await fs.cat(atPath: envFile)) ?? Data(), encoding: .utf8),
-               contents == oldEnvFish(ctx)
+               contents == oldEnvFish(ctx) || contents == oldEnvFish2(ctx)
             {
                 await ctx.print("Updating fish shell environment \(envFile)")
                 try Data(envFish(ctx).utf8).write(to: envFile, options: .atomic)
@@ -240,14 +277,14 @@ struct Init: SwiftlyCommand {
             sourceLine = """
 
             # Added by swiftly
-            source "\(envFile)"
+            source \(Swiftly.currentPlatform.escapePathForShell(envFile))
             """
         } else {
             envFile = Swiftly.currentPlatform.swiftlyHomeDir(ctx) / "env.sh"
             sourceLine = """
 
             # Added by swiftly
-            . "\(envFile)"
+            . \(Swiftly.currentPlatform.escapePathForShell(envFile))
             """
         }
 
